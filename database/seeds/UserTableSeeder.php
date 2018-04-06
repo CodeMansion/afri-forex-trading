fc631d16ff91aee69b183d6009c2329c117483e4
Clement Jegede <adek4life@gmail.com>
2018-04-06 04:38:37 +0100

    pulled from ajax-local

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -11,6 +11,7 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
+        \DB::statement("SET FOREIGN_KEY_CHECKS = 0");
         \DB::table("users")->truncate();
         \App\User::insert([
             [
